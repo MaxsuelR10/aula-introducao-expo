95af9430c81b102c6b0865257223685150e35ff8
MaxsuelR10 <maxsuelrodriguessantos15@gmail.com>
2025-08-15 22:45:41 -0300

    Começando Exemplo01

---
+++ src/exemplos/ex01/index.js
@@ -0,0 +1,10 @@
+import { View, Text} from 'react-native';
+
+export default function Exemplo01() {
+    return(
+        <View>
+            <Text>Exemplo 1</Text>
+            <Text>iNTRODUÇÃO React Native com Expo</Text>
+        </View>
+    );
+}
